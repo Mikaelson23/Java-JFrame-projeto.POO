7738fccfcb91f31189c7cabd5a30c65178d8b7cd
Mikaelson23 <81366840+Mikaelson23@users.noreply.github.com>
2021-07-07 15:35:30 -0300

    Update CardapioI.java

--- src/CardapioI.java
+++ src/CardapioI.java
@@ -13,10 +13,22 @@ public interface CardapioI {
 
     public List<Comida> mostrarCardapio();
 
-    public void recuperarDados() throws IOException;
+    public List<Bebida> mostrarCardapioBebida();
+
+    public void gravarDados();
+
+    public void recuperarDados();
 
     public String mostrarComidas();
 
     public String mostrarBebidas();
 
+    public Comida pesquisaComidaPorNome(String nome);
+
+    public Bebida pesquisaBebidaPorNome(String nome);
+
+    public boolean removerComida(Comida comida);
+
+    public boolean removerBebida(Comida comida);
+    
 }
